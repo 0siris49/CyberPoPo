f10e1926b19e716d0fdc1cb96ea7b056cd6e3a71
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-04-19 15:59:31 +0800

    Update loginEntity.js
    Allowed for error message flow from Entity to Controller

--- js/loginEntity.js
+++ js/loginEntity.js
@@ -1,4 +1,12 @@
 import { checkEmailPassType } from "./firebase.js";
+import { setControllerMessage } from "./loginController.js";
+var message = "";
+
+export function setEntityMessage(messageFromFirebase){
+    message = messageFromFirebase;
+    console.log(message,'from Entity');
+    setControllerMessage(messageFromFirebase);
+}
 
 export function loginUser(arg){
     var email = arg.userEmail;
@@ -6,4 +14,13 @@ export function loginUser(arg){
     var type = arg.userType;
 
     checkEmailPassType(email, password, type);
+    
 }
+
+export function getEntityMessage(){
+    return message;
+}
+
+
+ 
+

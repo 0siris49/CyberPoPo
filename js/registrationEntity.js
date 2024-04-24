d0f878919d79c2b20b5ef0d8179ef6e1483684a2
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-04-24 22:12:51 +0800

    Update registrationEntity.js

--- js/registrationEntity.js
+++ js/registrationEntity.js
@@ -4,8 +4,13 @@ export function registerUser(arg){
     var email = arg.userEmail;
     var password = arg.userPass;
     var type = arg.userType;
-
-    createToFireBase(auth,email,password,type);
+    var dayDOB = arg.dayDOB;
+    var monthDOB = arg.monthDOB;
+    var yearDOB = arg.yearDOB;
+    var firstName = arg.firstName;
+    var lastName = arg.lastName;
+    var phoneNum = arg.phoneNum;
+    createToFireBase(auth,email,password,type,dayDOB,monthDOB,yearDOB,firstName,lastName,phoneNum);
     
 }
 

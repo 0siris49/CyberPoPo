905a2007c5fee62d655dbdcd3d04de361f378423
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-04-24 22:12:35 +0800

    Update registrationController.js

--- js/registrationController.js
+++ js/registrationController.js
@@ -2,10 +2,16 @@
 import { registerUser } from "./registrationEntity.js";
 
 class User {
-    constructor (userEmail, userPass, userType){
+    constructor (userEmail, userPass, userType, dayDOB, monthDOB, yearDOB,firstName,lastName,phoneNum){
         this.userEmail = userEmail;
         this.userPass = userPass;
         this.userType = userType;
+        this.dayDOB = dayDOB;
+        this.monthDOB = monthDOB;
+        this.yearDOB = yearDOB;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
     }
 }//Creating User Object
 
@@ -14,11 +20,25 @@ document.getElementById('regBtn').addEventListener('click',function(event){
     event.preventDefault();
     const userEmail = document.getElementById('regEmail').value;
     const userPass = document.getElementById('regPassword').value;
-    const userType = document.getElementById('role').value;
-
-    let newUser = new User(userEmail,userPass,userType);
+    const dayDOB = document.getElementById('regDobDay').value;
+    const monthDOB = document.getElementById('regDobMonth').value;
+    const yearDOB = document.getElementById('regDobYear').value;
+    const firstName = document.getElementById('regFName').value;
+    const lastName = document.getElementById('regLName').value;
+    const phoneNum = document.getElementById('regNum').value;
+    var userType = '';
+    if(document.getElementById('buyer').checked){
+        const userRole = document.getElementById('buyer').value;
+        userType = userRole;
+    }else if(document.getElementById('seller').checked){
+        const userRole = document.getElementById('seller').value;
+        userType = userRole;
+    }else if(document.getElementById('REA').checked){
+        const userRole = document.getElementById('REA').value;
+        userType = userRole;
+    }
+    let newUser = new User(userEmail,userPass,userType,dayDOB,monthDOB,yearDOB,firstName,lastName,phoneNum);
     
-    //storeByUserType(newUser);
     registerUser(newUser);
 })
 //Registers user to Firebase

74f583aa663d8a74085b482c1cf59f0813f88b62
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-04-28 05:28:17 +0800

    Update sellerController.js
    Allowed controller to update webpage upon new Property Listings

--- js/sellerController.js
+++ js/sellerController.js
@@ -17,4 +17,67 @@ export class sellerController{
         document.getElementById('sellerName').innerHTML=displayUserName;
     }
 
+    setSellerPropList(string){
+        //console.log(string,"From seller controller");
+        var stringArr = string.split("---");
+        var totalPropList = stringArr.length - 1;
+        //console.log(totalPropList);
+        const table = document.getElementById('table');
+        var cleanID = [];
+        var cleanPropName = [];
+        var cleanAgent = [];
+
+
+        for(let i=0; i<totalPropList;i++){
+            var splitToAttri = stringArr[i].split(",");
+            var splitToAttriCount = splitToAttri.length;
+
+
+            for(let x=0; x<splitToAttriCount;x++){
+                var attriString = splitToAttri[x].toString();
+                var removeEtc = attriString.replace(/['"{}]+/g, '');
+                //console.log(attriString);
+                
+                if(removeEtc.search("propertyID") != -1){
+                    var result = removeEtc.substring(removeEtc.lastIndexOf(":")+1);
+                    //console.log(result);
+                    cleanID.push(result);
+                }
+                
+                if(removeEtc.search("propertyName") != -1){
+                    var result = removeEtc.substring(removeEtc.lastIndexOf(":")+1);
+                    //console.log(result);
+                    cleanPropName.push(result);
+                }
+                 if(removeEtc.search("propertyAgent") != -1){
+                    var result = removeEtc.substring(removeEtc.lastIndexOf(":")+1);
+                    ///console.log(result);
+                    cleanAgent.push(result);
+                }
+            }
+        }
+
+
+        for(let x=0; x<totalPropList; x++){
+            //console.log(cleanID[x]);
+            ///console.log(cleanPropName[x]);
+            ///console.log(cleanAgent[x]);
+        }
+
+        for(let i=0; i<totalPropList;i++){
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${cleanID[i]}</td>
+            <td><a href="www.google.com"> <div>${cleanPropName[i]}</div>  </a></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>${cleanAgent[i]}</td>`;
+            table.appendChild(row);
+        }
+
+
+    }
+
+
+ 
 }

5c369d8f9323e5b7c4389d4f39cd69ec8ea7b460
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-04-29 04:47:49 +0800

    Update createPLPageController.js
    Takes in Agent Email

--- js/createPLPageController.js
+++ js/createPLPageController.js
@@ -2,17 +2,18 @@ import { reaEntity } from "./userEntity.js";
 
 export class createPropertyListingObject{
     constructor(propName,propLocation,propType,propDesc,propPrice,propBedroom,propBathroom,propSize,propYearBuilt,propAgent
-        ,propSellerEmail,propAgentID){
+        ,propAgentEmail,propSellerEmail,propAgentID){
             this.propName = propName;
             this.propLocation = propLocation;
             this.propType = propType;
             this.propDesc = propDesc;
             this.propPrice = propPrice;
             this.propBedroom = propBedroom;
-            this.propBathroom = propBathroom;
+            this.propBathroom = propBathroom; 
             this.propSize = propSize;
             this.propYearBuilt = propYearBuilt;
             this.propAgent = propAgent;
+            this.propAgentEmail = propAgentEmail;
             this.propSellerEmail = propSellerEmail;
             this.propAgentID = propAgentID;
         }
@@ -39,13 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
             const propSize = document.getElementById('property_size').value;
             const propYearBuilt = document.getElementById('year_built').value;
             const propAgent = document.getElementById('property_agent').value;
+            const propAgentEmail = document.getElementById('property_email').value;
             const propSellerEmail = document.getElementById('property_seller').value;
             const propAgentID = document.getElementById('agent_id').value;
 
-            console.log(propName,propLocation,propType,propDesc,propPrice,propBedroom,propBathroom,propSize,propYearBuilt,propAgent
-            ,propSellerEmail,propAgentID);
 
-            let newPL = new createPropertyListingObject(propName,propLocation,propType,propDesc,propPrice,propBedroom,propBathroom,propSize,propYearBuilt,propAgent,propSellerEmail,propAgentID);
+            let newPL = new createPropertyListingObject(propName,propLocation,propType,propDesc,propPrice,propBedroom,propBathroom,propSize,propYearBuilt,propAgent,propAgentEmail,propSellerEmail,propAgentID);
             let initReaEntity = new reaEntity();
             initReaEntity.createPropertyListingToFirebase(newPL);
         });

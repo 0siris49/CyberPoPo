337834ae6c0e342f50bd642e7d4b587f0a63002a
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-05-01 03:27:37 +0800

    Update sellerController.js
    Consistency purposes

--- js/sellerController.js
+++ js/sellerController.js
@@ -3,7 +3,7 @@ import { sellerEntity } from "./userEntity.js";
 window.onload = function() {
     let initSellerController = new sellerController();
     initSellerController.getUserToController();
-  };
+  }; 
 
 export class sellerController{
     //Actually calls for Firebase to check whether there's a user logged in, and if it does, return their displayName
@@ -21,12 +21,14 @@ export class sellerController{
         //console.log(string,"From seller controller");
         var stringArr = string.split("---");
         var totalPropList = stringArr.length - 1;
-        //console.log(totalPropList);
         const table = document.getElementById('table');
         var cleanID = [];
         var cleanPropName = [];
         var cleanAgent = [];
         var cleanStatus = [];
+        var cleanAvgRating = [];
+        var resultRating = 0;
+        var ratingCount = 0;
 
 
         for(let i=0; i<totalPropList;i++){
@@ -62,6 +64,20 @@ export class sellerController{
                     ///console.log(result);
                     cleanStatus.push(result);
                 }
+
+                if(removeEtc.search("propRating") != -1){
+                    resultRating = removeEtc.substring(removeEtc.lastIndexOf(":")+1);
+                    
+                    
+                }
+                if(removeEtc.search("propRTC") != -1){
+                    ratingCount = removeEtc.substring(removeEtc.lastIndexOf(":")+1);
+                }
+                if(resultRating == 0 && ratingCount == 0 ){
+                    cleanAvgRating.push("No Ratings Yet");
+                }else{
+                    cleanAvgRating.push(resultRating/ratingCount);
+                }
             }
         }
 
@@ -70,13 +86,14 @@ export class sellerController{
             //console.log(cleanID[x]);
             ///console.log(cleanPropName[x]);
             ///console.log(cleanAgent[x]);
+            
         }
 
         for(let i=0; i<totalPropList;i++){
             const row = document.createElement('tr');
             row.innerHTML = `<td>${cleanID[i]}</td>
-            <td><a href="www.google.com"> <div>${cleanPropName[i]}</div>  </a></td>
-            <td></td>
+            <td><a href="fullPropertyDetails.html?propertyId=${cleanID[i]}"> <div>${cleanPropName[i]}</div>  </a></td>
+            <td>${cleanAvgRating[i]}</td>
             <td></td>
             <td>${cleanStatus[i]}</td>
             <td>${cleanAgent[i]}</td>`;

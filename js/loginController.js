872465ad2f59bd12319180836aba86959c13f50f
binsong <binsongxiong@gmail.com>
2024-05-03 20:42:36 +0800

    some minor visual update

--- js/loginController.js
+++ js/loginController.js
@@ -3,26 +3,26 @@ import { loginEntity } from "./userEntity.js";
 var controllerMessage = "";
 
 export class User {
-    constructor (userEmail, userPass, userType){
+    constructor(userEmail, userPass, userType) {
         this.userEmail = userEmail;
         this.userPass = userPass;
         this.userType = userType;
     }
 
-    setControllerMessage(entityMessage){
+    setControllerMessage(entityMessage) {
         controllerMessage = entityMessage;
-        console.log(controllerMessage,"from Controller");
-        if(controllerMessage != ""){
+        console.log(controllerMessage, "from Controller");
+        if (controllerMessage != "") {
             alert(controllerMessage);
         }
     }
-    
+
 }
 
-export function setControllerMessage(entityMessage){
+export function setControllerMessage(entityMessage) {
     controllerMessage = entityMessage;
-    console.log(controllerMessage,"from Controller");
-    if(controllerMessage != ""){
+    console.log(controllerMessage, "from Controller");
+    if (controllerMessage != "") {
         alert(controllerMessage);
     }
 }
@@ -39,8 +39,37 @@ document.addEventListener("DOMContentLoaded", () => {
             let newUser = new User(userEmail, userPass, userType);
             let initLoginEntity = new loginEntity();
             initLoginEntity.loginToFirebase(newUser);
-            
+
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginBtn = document.getElementById('loginBtn');
+    const emailInput = document.getElementById('loginEmail');
+    const passwordInput = document.getElementById('loginPass');
+
+    loginBtn.addEventListener('click', function (event) {
+        let hasEmptyFields = false;
+        if (emailInput.value.trim() === "") {
+            emailInput.style.border = "2px solid red";
+            hasEmptyFields = true;
+        } else {
+            emailInput.style.border = "";
+        }
+        if (passwordInput.value.trim() === "") {
+            passwordInput.style.border = "2px solid red";
+            hasEmptyFields = true;
+        } else {
+            passwordInput.style.border = "";
+        }
+
+        if (hasEmptyFields) {
+            event.preventDefault();
+            alert('Please fill out all fields.');
+        }
+    });
+});
+
+
 //Login system
--- js/registrationController.js
+++ js/registrationController.js
@@ -1,8 +1,8 @@
 //Registration Controller
 
-import {registrationEntity} from "./userEntity.js";
+import { registrationEntity } from "./userEntity.js";
 class User {
-    constructor (userEmail, userPass, userType, dayDOB, monthDOB, yearDOB,firstName,lastName,phoneNum){
+    constructor(userEmail, userPass, userType, dayDOB, monthDOB, yearDOB, firstName, lastName, phoneNum) {
         this.userEmail = userEmail;
         this.userPass = userPass;
         this.userType = userType;
@@ -16,7 +16,7 @@ class User {
 }//Creating User Object
 
 
-document.getElementById('regBtn').addEventListener('click',function(event){
+document.getElementById('regBtn').addEventListener('click', function (event) {
     event.preventDefault();
     const userEmail = document.getElementById('regEmail').value;
     const userPass = document.getElementById('regPassword').value;
@@ -27,17 +27,49 @@ document.getElementById('regBtn').addEventListener('click',function(event){
     const lastName = document.getElementById('regLName').value;
     const phoneNum = document.getElementById('regNum').value;
     var userType = '';
-    if(document.getElementById('buyer').checked){
+    if (document.getElementById('buyer').checked) {
         const userRole = document.getElementById('buyer').value;
         userType = userRole;
-    }else if(document.getElementById('seller').checked){
+    } else if (document.getElementById('seller').checked) {
         const userRole = document.getElementById('seller').value;
         userType = userRole;
-    }else if(document.getElementById('REA').checked){
+    } else if (document.getElementById('REA').checked) {
         const userRole = document.getElementById('REA').value;
         userType = userRole;
     }
-    let newUser = new User(userEmail,userPass,userType,dayDOB,monthDOB,yearDOB,firstName,lastName,phoneNum);
+    let newUser = new User(userEmail, userPass, userType, dayDOB, monthDOB, yearDOB, firstName, lastName, phoneNum);
     let newEntity = new registrationEntity(newUser);
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const regBtn = document.getElementById('regBtn');
+    const fName = document.getElementById('regFName');
+    const lName = document.getElementById('regLName');
+    const email = document.getElementById('regEmail');
+    const phone = document.getElementById('regNum');
+    const password = document.getElementById('regPassword');
+    const dobDay = document.getElementById('regDobDay');
+    const dobMonth = document.getElementById('regDobMonth');
+    const dobYear = document.getElementById('regDobYear');
+
+    regBtn.addEventListener('click', function(event) {
+        let hasEmptyFields = false;
+        let fields = [fName, lName, email, phone, password, dobDay, dobMonth, dobYear];
+
+        fields.forEach(field => {
+            if (field.value.trim() === "") {
+                field.style.border = "2px solid red";
+                hasEmptyFields = true;
+            } else {
+                field.style.border = "";
+            }
+        });
+
+        if (hasEmptyFields) {
+            event.preventDefault(); // Prevent form submission
+            alert('Please fill out all required fields.');
+        }
+    });
+});
+
 //Registers user to Firebase

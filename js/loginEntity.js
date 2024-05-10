03c6666690b8d3700c742e6ac4174ebe30f5f58c
Xerawastaken <60858398+Xeraaaaaaa@users.noreply.github.com>
2024-05-10 18:33:48 +0800

    Controller and Entity Split Part 3

---
+++ js/loginEntity.js
@@ -0,0 +1,26 @@
+/*import { checkEmailPassType } from "./firebase.js";
+import { setControllerMessage } from "./loginController.js";
+var message = "";
+
+export function setEntityMessage(messageFromFirebase){
+    message = messageFromFirebase;
+    console.log(message,'from Entity');
+    setControllerMessage(messageFromFirebase);
+}
+
+export function loginUser(arg){
+    var email = arg.userEmail;
+    var password = arg.userPass;
+    var type = arg.userType;
+
+    checkEmailPassType(email, password, type);
+    
+}
+
+export function getEntityMessage(){
+    return message;
+}*/
+
+
+ 
+
